e9c2fcb6b5c2631b736bb4b131fd79766bcf2cc8
wasalas <wasalas893@gmail.com>
2019-11-17 03:05:23 +0530

    add to appjs

---
+++ app.js
@@ -0,0 +1,17 @@
+//book construction
+function Book(title,author,isbn){
+    this.title=title;
+    this.author=author;
+    this.isbn=isbn;
+
+}
+//ui Contortor
+function UI(){
+
+}
+//event Listeners
+document.getElementById('book-from').addEventListener('submit',
+  function(e){
+      e.preventDefault();
+  }
+);
