b03720146d269e1b1d5544db4a0ec10c4fe6f6de
wasalas <wasalas893@gmail.com>
2019-11-19 21:32:56 +0530

    appjs

--- app.js
+++ app.js
@@ -12,13 +12,15 @@ function UI(){
 //event Listeners
 document.getElementById('book-from').addEventListener('submit',
   function(e){
+    // get form values
+
+    const title=document.getElementById('title').value,
+    author=document.getElementById('author').value,
+    isbn=document.getElementById('isbn').value
+    // instantiate book
+    const book=new Book(title,author,isbn);
+    console.log(book);
       e.preventDefault();
   }
 );
 
-function UI(){
-  this.title=title;
-    this.author=author;
-    this.isbn=isbn;
-
-}
